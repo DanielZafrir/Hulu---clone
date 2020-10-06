e586f2ee938a3e4098776cdd8c109fb1942165b8
DanielZafrir <67580257+DanielZafrir@users.noreply.github.com>
2020-10-06 10:32:46 +0300

    Add files via upload

---
+++ components/Results/Results.js
@@ -0,0 +1,33 @@
+import React, { useState, useEffect } from "react";
+
+import "./Results.css";
+
+import VideoCard from "./VideoCard/VideoCard";
+import axios from "../../axios";
+import FlipMove from "react-flip-move";
+
+const Results = ({ selectedOption }) => {
+  const [movies, setMovies] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const request = await axios.get(selectedOption);
+      setMovies(request.data.results);
+      return request;
+    };
+
+    fetchData();
+  }, [selectedOption]);
+
+  return (
+    <div className="results">
+      <FlipMove>
+        {movies.map((movie) => (
+          <VideoCard key={movie.id} movie={movie} />
+        ))}
+      </FlipMove>
+    </div>
+  );
+};
+
+export default Results;
